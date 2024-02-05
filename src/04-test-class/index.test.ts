316948427bbdc4db21c9ac64b65ec2faf051c1f5
Alina Nosovets <alinanosovets6849@gmail.com>
2024-02-05 22:36:33 +0700

    feat: partially implement test class task

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,33 +1,66 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import {
+  BankAccount,
+  getBankAccount,
+  InsufficientFundsError,
+  TransferFailedError,
+  // SynchronizationFailedError,
+} from './index';
 
 describe('BankAccount', () => {
+  let myBankAccount: BankAccount;
+  let strangerAccount: BankAccount;
+  let myBalance: number;
+
+  beforeAll(() => {
+    myBankAccount = getBankAccount(100);
+    strangerAccount = getBankAccount(100);
+  });
+
+  beforeEach(() => {
+    myBalance = myBankAccount.getBalance();
+  });
+
   test('should create account with initial balance', () => {
-    // Write your test here
+    expect(myBankAccount.getBalance()).toBe(100);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    if (1000 > myBalance) {
+      expect(() => myBankAccount.withdraw(1000)).toThrow(
+        InsufficientFundsError,
+      );
+    }
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    if (1000 > myBalance) {
+      expect(() => myBankAccount.transfer(1000, strangerAccount)).toThrow();
+    }
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    expect(() => myBankAccount.transfer(100, myBankAccount)).toThrow(
+      TransferFailedError,
+    );
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    myBankAccount.deposit(2000);
+    myBalance += 2000;
+    expect(myBankAccount.getBalance()).toBe(myBalance);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    myBankAccount.withdraw(50);
+    myBalance -= 50;
+    expect(myBankAccount.getBalance()).toBe(myBalance);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    myBankAccount.transfer(50, strangerAccount);
+    myBalance -= 50;
+    expect(myBankAccount.getBalance()).toBe(myBalance);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
