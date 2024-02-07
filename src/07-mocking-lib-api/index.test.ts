f57865c8ba0d30954a4aec81a4b2c802bbd578d4
Alina Nosovets <alinanosovets6849@gmail.com>
2024-02-07 16:16:43 +0700

    feat: implement first test in throttledGetDataFromApi

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,10 +1,35 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios', () => ({
+  create: jest.fn(() => ({
+    get: jest.fn().mockResolvedValue({ data: 'mockData' }),
+  })),
+}));
+
+jest.mock('lodash/throttle', () => ({
+  default: jest.fn((fn) => fn),
+  __esModule: true,
+}));
 
 describe('throttledGetDataFromApi', () => {
+  beforeEach(() => {
+    (axios.create as jest.Mock).mockReturnValue({
+      get: jest.fn().mockResolvedValue({ data: 'mockData' }),
+    });
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    await throttledGetDataFromApi('/posts');
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
